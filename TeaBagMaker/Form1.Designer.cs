012e472b3afd003652c1a840207f293be6176975
soyeon207 <psy010207@naver.com>
2019-05-27 09:28:28 +0900

    #1 폼 디자인

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -41,14 +41,14 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(13, 13);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(283, 20);
+            this.comboBox1.Size = new System.Drawing.Size(296, 20);
             this.comboBox1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(13, 40);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(283, 26);
+            this.button1.Size = new System.Drawing.Size(296, 26);
             this.button1.TabIndex = 1;
             this.button1.Text = "담그기!";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,18 +56,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label1.Location = new System.Drawing.Point(11, 69);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.Size = new System.Drawing.Size(50, 14);
             this.label1.TabIndex = 2;
             this.label1.Text = "시간 : ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label2.Location = new System.Drawing.Point(199, 133);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(133, 12);
+            this.label2.Size = new System.Drawing.Size(171, 14);
             this.label2.TabIndex = 3;
             this.label2.Text = "2019-05-27 3506 박소연";
             // 
