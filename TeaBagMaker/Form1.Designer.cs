7a3959f55251a2c12043b2279e5d26fd3683b840
soyeon207 <psy010207@naver.com>
2019-05-27 09:44:53 +0900

    #2 콤보박스로 티 종류 선택 & 시간 라벨 표시

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -29,39 +29,44 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.CB = new System.Windows.Forms.ComboBox();
+            this.btn = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
+            this.timelbl = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // CB
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(13, 13);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(296, 20);
-            this.comboBox1.TabIndex = 0;
+            this.CB.FormattingEnabled = true;
+            this.CB.Location = new System.Drawing.Point(13, 13);
+            this.CB.Name = "CB";
+            this.CB.Size = new System.Drawing.Size(296, 20);
+            this.CB.TabIndex = 0;
+            this.CB.SelectedIndexChanged += new System.EventHandler(this.CB_SelectedIndexChanged);
             // 
-            // button1
+            // btn
             // 
-            this.button1.Location = new System.Drawing.Point(13, 40);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(296, 26);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "담그기!";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn.Location = new System.Drawing.Point(13, 40);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(296, 26);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "담그기!";
+            this.btn.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // timer1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(11, 69);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 14);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "시간 : ";
+            this.timer1.Tag = "timer";
+            // 
+            // timelbl
+            // 
+            this.timelbl.AutoSize = true;
+            this.timelbl.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.timelbl.Location = new System.Drawing.Point(11, 69);
+            this.timelbl.Name = "timelbl";
+            this.timelbl.Size = new System.Drawing.Size(50, 14);
+            this.timelbl.TabIndex = 2;
+            this.timelbl.Text = "시간 : ";
             // 
             // label2
             // 
@@ -77,13 +82,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(344, 154);
+            this.ClientSize = new System.Drawing.Size(371, 165);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.timelbl);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.CB);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "TeaBagMaker";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -92,10 +97,10 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox CB;
+        private System.Windows.Forms.Button btn;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label timelbl;
         private System.Windows.Forms.Label label2;
     }
 }
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -11,6 +11,11 @@ namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
+        string[] tea = new string[] { "홍차", "녹차", "루이보스차", "국화차" };
+        int[] tea_time = new int[] { 120, 180, 300, 120 };
+        string txt;
+        int sel;
+
         public Form1()
         {
             InitializeComponent();
@@ -18,7 +23,28 @@ namespace TeaBagMaker
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            for(int i=0;i<tea.Length;i++)
+            {
+                this.CB.Items.Add(tea[i]);
+            }
+            this.txt = timelbl.Text;
+            
+        }
 
+        private void CB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            timelbl.Text = CB.Text;
+
+            for(int i=0;i<tea.Length;i++)
+            {
+                if (CB.Text.Equals(tea[i]))
+                {
+                    sel = i;
+                    break;
+                }
+            }
+
+            timelbl.Text = txt + tea_time[sel] / 60 + "분";
         }
     }
 }
